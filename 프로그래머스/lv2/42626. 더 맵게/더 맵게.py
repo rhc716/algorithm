729494acc34cf61c38087b814bbd6af4756bc9a1
hyeon chol Ro <jack716@naver.com>
2023-01-03 16:18:52 +0900

    [level 2] Title: 더 맵게, Time: 1670.69 ms, Memory: 51.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,11 @@
+import heapq
+
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    answer = 0
+    while scoville[0] < K:
+        if len(scoville) == 1:
+            return -1
+        heapq.heappush(scoville,heapq.heappop(scoville)+heapq.heappop(scoville)*2)
+        answer += 1
+    return answer
