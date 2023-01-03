2bc5765772a701770001e1bcf4fdc05e9a0bdf6f
hyeon chol Ro <jack716@naver.com>
2023-01-03 17:22:15 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+def solution(array):
+    while len(array) != 0:
+        for i, a in enumerate(set(array)):
+            print(a)
+            array.remove(a)
+        if i == 0:
+            return a
+    return -1
